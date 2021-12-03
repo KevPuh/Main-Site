d071e9bc20ea73f898d9d3efbd5061fb43f8014c
KevPuh <56805332+KevPuh@users.noreply.github.com>
2021-12-03 11:32:35 +0100

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,378 @@
+document.getElementById('bg-task').addEventListener("click", hide);
+
+    function blockAll(element){
+      let elements = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","w","x","y","z"];
+      elements.forEach(char => {
+        if(element != char){
+          document.getElementById(char).style.display = "none";
+        }
+      });
+      document.getElementById('bg-task').style.display = "block";
+      document.getElementById(element).style.display = "block";
+    }
+
+    function hide() {
+      document.getElementById('bg-task').style.display = "none";
+      $('#bg-task>div').hide();
+    }
+
+    document.getElementById('1').addEventListener('click', a);
+
+    function a() {
+      let GivenDate = '2021-12-01';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("a");
+      }
+    }
+
+
+    document.getElementById('2').addEventListener('click', b);
+
+    function b() {
+      let GivenDate = '2021-12-02';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("b");
+      }
+    }
+
+    document.getElementById('3').addEventListener('click', c);
+
+    function c() {
+      let GivenDate = '2021-12-03';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("c");
+      }
+    }
+
+    document.getElementById('4').addEventListener('click', d);
+
+    function d() {
+      let GivenDate = '2021-12-04';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("d");
+      }
+    }
+
+    document.getElementById('5').addEventListener('click', e);
+
+    function e() {
+      let GivenDate = '2021-12-05';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("e");
+      }
+    }
+
+    document.getElementById('6').addEventListener('click', f);
+
+    function f() {
+      let GivenDate = '2021-12-06';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("f");
+      }
+    }
+
+    document.getElementById('7').addEventListener('click', g);
+
+    function g() {
+      let GivenDate = '2021-12-07';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("g");
+      }
+    }
+
+    document.getElementById('8').addEventListener('click', h);
+
+    function h() {
+      let GivenDate = '2021-12-08';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("h");
+      }
+    }
+
+    document.getElementById('9').addEventListener('click', i);
+
+    function i() {
+      let GivenDate = '2021-12-09';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("i");
+      }
+    }
+
+    document.getElementById('10').addEventListener('click', j);
+
+    function j() {
+      let GivenDate = '2021-12-10';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("j");
+      }
+    }
+
+    document.getElementById('11').addEventListener('click', k);
+
+    function k() {
+      let GivenDate = '2021-12-11';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("k");
+      }
+    }
+
+    document.getElementById('12').addEventListener('click', l);
+
+    function l() {
+      let GivenDate = '2021-12-12';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("l");
+      }
+    }
+
+    document.getElementById('13').addEventListener('click', m);
+
+    function m() {
+      let GivenDate = '2021-12-13';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("m");
+      }
+    }
+
+    document.getElementById('14').addEventListener('click', n);
+
+    function n() {
+      let GivenDate = '2021-12-14';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("n");
+      }
+    }
+
+    document.getElementById('15').addEventListener('click', o);
+
+    function o() {
+      let GivenDate = '2021-12-15';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("o");
+      }
+    }
+
+    document.getElementById('16').addEventListener('click', p);
+
+    function p() {
+      let GivenDate = '2021-12-16';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("p");
+      }
+    }
+
+    document.getElementById('17').addEventListener('click', r);
+
+    function r() {
+      let GivenDate = '2021-12-17';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("r");
+      }
+    }
+
+    document.getElementById('18').addEventListener('click', s);
+
+    function s() {
+      let GivenDate = '2021-12-18';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("s");
+      }
+    }
+
+    document.getElementById('19').addEventListener('click', t);
+
+    function t() {
+      let GivenDate = '2021-12-19';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("t");
+      }
+    }
+
+    document.getElementById('20').addEventListener('click', u);
+
+    function u() {
+      let GivenDate = '2021-12-20';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("u");
+      }
+    }
+
+    document.getElementById('21').addEventListener('click', w);
+
+    function w() {
+      let GivenDate = '2021-12-21';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("w");
+      }
+    }
+
+    document.getElementById('22').addEventListener('click', x);
+
+    function x() {
+      let GivenDate = '2021-12-22';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("x");
+      }
+    }
+
+    document.getElementById('23').addEventListener('click', y);
+
+    function y() {
+      let GivenDate = '2021-12-23';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("y");
+      }
+    }
+
+    document.getElementById('24').addEventListener('click', z);
+
+    function z() {
+      let GivenDate = '2021-12-24';
+      let CurrentDate = new Date();
+      GivenDate = new Date(GivenDate);
+
+      if (GivenDate > CurrentDate) {
+        alert(" Don't be so impatient! This window will open soon.");
+      }
+      else {
+        blockAll("z");
+      }
+    }
